7a434a400eea9bceecdefd7ab33e8d313ad40d82
userbyte <42632711+userbyte@users.noreply.github.com>
2022-10-28 13:54:53 -0400

    add fetch page
    spent way too long on this lmao

---
+++ files/js/fetch.js
@@ -0,0 +1,60 @@
+/* ion know how this shit works but it does: https://dev.to/shantanu_jana/how-to-create-a-draggable-div-in-javascript-iff */
+const dragElement = (element, dragzone) => {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  //MouseUp occurs when the user releases the mouse button
+  const dragMouseUp = () => {
+    document.onmouseup = null;
+    //onmousemove attribute fires when the pointer is moving while it is over an element.
+    document.onmousemove = null;
+    element.classList.remove("drag");
+  };
+
+  const dragMouseMove = (event) => {
+    event.preventDefault();
+  
+    //clientX property returns the horizontal coordinate of the mouse pointer
+    pos1 = pos3 - event.clientX;
+
+    //clientY property returns the vertical coordinate of the mouse pointer
+    pos2 = pos4 - event.clientY;
+    pos3 = event.clientX;
+    pos4 = event.clientY;
+
+    //offsetTop property returns the top position relative to the parent
+    element.style.top = `${element.offsetTop - pos2}px`;
+    element.style.left = `${element.offsetLeft - pos1}px`;
+  };
+
+  const dragMouseDown = (event) => {
+    event.preventDefault();
+
+    pos3 = event.clientX;
+    pos4 = event.clientY;
+
+    element.classList.add("drag");
+
+    document.onmouseup = dragMouseUp;
+    document.onmousemove = dragMouseMove;
+  };
+
+  dragzone.onmousedown = dragMouseDown;
+};
+
+
+window.onload = function onLoad() {
+  dragable = document.getElementById("terminal");
+  dragzone = document.getElementById("titlebar");
+  dragElement(dragable, dragzone);
+
+  document.getElementById("term-input").addEventListener("keyup", event => {
+    if (event.key !== "Enter") return;
+    if (document.getElementById('term-input').value === '') return;
+    console.log(`term input received: ${document.getElementById('term-input').value}`);
+    //handleTerminalInput(document.getElementById('term-input').value)
+    document.getElementById('term-input').value = '';
+  });
+}
