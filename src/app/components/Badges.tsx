c0e5853ad12d89c9fcb1e62a0290fb96c4d2574a
userbyte <42632711+userbyte@users.noreply.github.com>
2026-08-25 19:30:39 -0400

    Badges: build list from objects

--- src/app/components/Badges.tsx
+++ src/app/components/Badges.tsx
@@ -1,24 +1,40 @@
 import styles from "@/app/style/modules/components/Badges.module.css";
 
+const badgeList: Array<{ link: string; desc: string; imgURL: string }> = [
+  {
+    link: "https://userbyte.xyz/",
+    desc: "userbyte.xyz",
+    imgURL: "/img/webp/88x31.webp",
+  },
+  {
+    link: "https://electron271.gay/",
+    desc: "electron271.gay",
+    imgURL: "https://electron271.gay/88x31/badgev1.webp",
+  },
+  {
+    link: "https://grainware.org/",
+    desc: "grainware.org",
+    imgURL: "https://electron271.gay/88x31/poweredbygrainware.avif",
+  },
+];
+
 export default function Badges() {
   return (
     <div className={styles.main}>
-      <a href="https://userbyte.xyz/" target="_blank">
-        <img
-          src="/img/webp/88x31.webp"
-          alt="userbyte.xyz"
-          width="88"
-          height="31"
-        />
-      </a>
-      <a href="https://electron271.gay/" target="_blank">
-        <img
-          src="https://electron271.gay/88x31/badgev1.webp"
-          alt="electron271.gay"
-          width="88"
-          height="31"
-        />
-      </a>
+      {badgeList.map(
+        (badge: { link: string; desc: string; imgURL: string }) => {
+          return (
+            <a
+              key={badge.link}
+              href={badge.link}
+              target="_blank"
+              title={badge.desc}
+            >
+              <img src={badge.imgURL} alt={badge.desc} width="88" height="31" />
+            </a>
+          );
+        }
+      )}
     </div>
   );
 }
