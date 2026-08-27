ce2b8440c6b2bfe41eeb09dff6d198d13125ebfc
userbyte <42632711+userbyte@users.noreply.github.com>
2026-08-27 01:21:50 -0400

    Badges: more badges

--- src/app/components/Badges.tsx
+++ src/app/components/Badges.tsx
@@ -7,14 +7,39 @@ const badgeList: Array<{ link: string; desc: string; imgURL: string }> = [
     imgURL: "/img/88x31/userbyte.webp",
   },
   {
+    link: "https://grainware.org/",
+    desc: "grainware.org",
+    imgURL: "https://electron271.gay/88x31/poweredbygrainware.avif",
+  },
+  {
+    link: "https://accuratelinuxgraphs.com/",
+    desc: "ACCURACY",
+    imgURL: "/img/88x31/accuracy.avif",
+  },
+  {
     link: "https://electron271.gay/",
     desc: "electron271.gay",
     imgURL: "https://electron271.gay/88x31/badgev1.webp",
   },
   {
-    link: "https://grainware.org/",
-    desc: "grainware.org",
-    imgURL: "https://electron271.gay/88x31/poweredbygrainware.avif",
+    link: "https://doclic.eu/",
+    desc: "doclic.eu",
+    imgURL: "/img/88x31/doclic.gif",
+  },
+  {
+    link: "https://bontboss.com/",
+    desc: "bontboss.com",
+    imgURL: "https://bontboss.com/pictures/bontboss.png",
+  },
+  {
+    link: "https://glocean.dev/",
+    desc: "glocean.dev",
+    imgURL: "/img/88x31/glocean.png",
+  },
+  {
+    link: "https://www.youtube.com/watch?v=FtutLA63Cp8",
+    desc: "of course someone put bad apple in an 88x31",
+    imgURL: "/img/88x31/bad_apple.webp",
   },
 ];
 
